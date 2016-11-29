5fe9c28925efb8f3f3756e85fd037f1d6ebe4e87
pnyxtr <se-staekva-01@SE-DEVPC1032>
2016-11-29 11:21:39 +0100

    better

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -4,12 +4,32 @@ namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Default C# folding player";
+		public static readonly string VERSION = "A little better player";
 
 		public static int BetRequest(JObject gameState)
 		{
-			//TODO: Use this method to return the value You want to bet
-			return 0;
+            var state = gameState.ToObject<MainState>();
+            var player = state.players[state.in_action];
+
+            var card1 = player.hole_cards[0];
+            var card2 = player.hole_cards[1];
+
+
+		    if (state.current_buy_in > 200)
+		    {
+		        var equalCards = card1.rank == card2.rank;
+		        var card1High = RankToValue.Convert(card1.rank) >= 10;
+                var card2High = RankToValue.Convert(card2.rank) >= 10;
+		        var card1Ace = RankToValue.Convert(card1.rank) >= 14;
+                var card2Ace = RankToValue.Convert(card2.rank) >= 14;
+
+                if (equalCards ||
+                    card1Ace && card2High ||
+                    card2Ace && card1High)
+		            return 10000;                
+		    }
+
+            return 0;
 		}
 
 		public static void ShowDown(JObject gameState)
---
+++ src/RankToValue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple
+{
+    public static class RankToValue
+    {
+        public static int Convert(string rank)
+        {
+            var r = rank.ToUpper();
+            if (r == "2")
+                return 2;
+            if (r == "3")
+                return 3;
+            if (r == "4")
+                return 4;
+            if (r == "5")
+                return 5;
+            if (r == "6")
+                return 6;
+            if (r == "7")
+                return 7;
+            if (r == "8")
+                return 8;
+            if (r == "9")
+                return 9;
+            if (r == "T")
+                return 10;
+            if (r == "J")
+                return 11;
+            if (r == "Q")
+                return 12;
+            if (r == "K")
+                return 13;
+            if (r == "A")
+                return 14;
+            return 0;
+        }
+    }
+}
